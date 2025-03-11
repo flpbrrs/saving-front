a5177b7b769746a53abb95edff6a37eeba86eecc
Felipe Barros <felipejonathan.dev@gmail.com>
2025-03-11 19:52:38 -0300

    feat: adicionando layouts de proteção de autenticação

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import useAuth from "@/hooks/useAuth"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    const { loggedUser } = useAuth()
+    const router = useRouter()
+
+    useEffect(() => {
+        if (loggedUser)
+            router.push('/home')
+    }, [loggedUser, router])
+
+    if (loggedUser)
+        return null
+
+    return <>{children}</>
+}
