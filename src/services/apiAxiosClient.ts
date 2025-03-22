cf3154425e659b87a5ea0a98fdc227c1264a065e
Felipe Barros <felipejonathan.dev@gmail.com>
2025-03-22 10:12:45 -0300

    chore: adicionando interceptor de autorização

--- src/services/apiAxiosClient.ts
+++ src/services/apiAxiosClient.ts
@@ -4,4 +4,27 @@ const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL!
 })
 
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("authToken");
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && error.response.status === 403) {
+            localStorage.removeItem("token");
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default api
